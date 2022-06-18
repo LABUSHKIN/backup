d52dc2e4387d10430d340d82605c862b3e1c5e5c
Sergey Lab <3403816@gmail.com>
2022-06-18 22:29:50 +0300

    fix: js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,143 @@
+console.log(typeof('1'+2));
 
+//console.log(typeof(NaN + 101));
+
+/*let a=5, b=4;
+console.log (const sum = (a, b) => { return a + b });*/   //фигня
+
+
+/*let message = 'Rokdrez'
+console.log( message[message.length - 1]);  // <=как вывести последний символ*/
+
+/*const RS = 'rs'; console.log('css', RS, 'js'); //=css rs js*/
+
+/*let name = 'Vasia'; name[3] = 'y'; console.log(name);  //=Vasia */
+
+//console.log(typeof(Infinity)); =number
+
+/*const max = (a, b) => (a == b ? a : b);
+const max = (a, b) => (a > b ? a : b);
+const max = (a, b) => (a + b > a - b ? a : b);
+const max = (a, b) => (a + b < a - b ? a : b);*/
+
+
+
+//console.log(typeof('4' * '9')); =number 36
+
+//let a = 5; console.log(++a); console.log(a++);
+
+/*let a = 18, b = 9; 
+console.log (Boolean(a % b === 0));*/
+
+//let i = 10; while (i < 10) {f+k};
+//console.log('Hey there!'
+
+//console.log(Math.abs(Math.floor(-46.867)));
+
+//console.log (typeof(1 / 0));
+
+/*let i = 0; let c = 0; 
+const str = 'Rolling Scopes School'; 
+while (i < str.length) { 
+    if (str[i] === 'o') {
+         c++; 
+        } i++; 
+    } 
+console.log(c);     // =4*/
+
+//let first = 1; let first = 2;
+//const first = 1, second = 2
+//console.log(first, second);
+//let first = 1; first = 2;
+//console.log(first);
+/*console.log(typeof(Math.min(10, 222, -5, 0, '-33')));*/  //-33   number 
+
+/*console.log(typeof( 'Hey' )); //все 7 шт. строки
+console.log(typeof( '' )); 
+console.log(typeof( 'undefined' )); 
+console.log(typeof( "Hello" )); 
+console.log(typeof( ' ' )); 
+console.log(typeof( " " )); 
+console.log(typeof( "null" )); */
+
+
+//console.log('после    '+'    до');
+
+//const showRS = () => arguments[0] + arguments[1]; console.log('RSSchool ' + showRS('2021', 'Q3')); 
+/* даааа *///const showRS = (year, quarter) => year + quarter; console.log('RSSchool ' + showRS('2021', 'Q3'));
+//const showRS = (text = year + quarter) => text; console.log('RSSchool ' + showRS('2021', 'Q3'));
+/* дааа */ //const showRS = (year, quarter) => {return year + quarter}; console.log('RSSchool ' + showRS('2021', 'Q3'));
+//const showRS = (text = year + quarter) => text; console.log('RSSchool ' + showRS('2021', 'Q3'));
+
+
+/*console.log(1 / 0); //   =infinity */
+
+/*console.log(Math.pow(2,4)); // =16*/
+
+/*a = true, b = true;
+console.log(Boolean ( a || b)); //true
+console.log(Boolean ( !a || b));  //true
+console.log(Boolean ( a || !b)); //true
+console.log(Boolean ( !a || !b)); //false
+console.log(Boolean ( !(a || b))); //false
+console.log(Boolean ( !(!a || !b))); //true*/
+
+/*a = true, b = true;
+console.log(Boolean (a && b )); +
+console.log(Boolean ( !a && b));
+console.log(Boolean ( a && !b));
+console.log(Boolean (!a && !b ));
+console.log(Boolean (!(a && b) ));
+console.log(Boolean (!(!a && !b) )); +*/
+
+/*const name = 'Vasia'; 
+console.log(Math.abs(name.length - 15));  // 10*/
+
+/*console.log (7 * 3 + (4 / 2) - (8 + (2 - 1))); //true
+console.log (7 * 3 + (4 / 2) - 8 + 2 - 1);
+console.log (7 * (3 + 4) / 2 - (8 + 2 - 1));
+console.log (7 * 3 + (4 / 2) - ((8 + 1) - 2));*/
+
+/*console.log (4**10);*/
+
+/*let a = 5; console.log(++a); console.log(a++);    //6 6*/
+
+/*console.log (0.32 * 0.22 * 0.75)*/
+
+/*console.log(Boolean (0 === 1) );
+console.log(Boolean (1 === '1') );
+console.log(Boolean (1 == '1'));
+console.log(Boolean (4 === 1 + 3) );
+console.log(Boolean ('A' > 'a') );
+console.log(Boolean ('B' < 'b') );
+console.log(Boolean ('13' > 12) );
+console.log(Boolean ('13px' > 12) );
+console.log(Boolean ('13px' < '14') );*/
+
+/*let name = 'Alex';
+console.log ('Hello ${name}'); //??? при выводе 2 пробела
+console.log('Hello ', name);
+console.log('Hello name');
+console.log( 'Hello ' + name);   //даааа
+console.log(`Hello ${name}`);  //даааа */
+
+
+/*let first = 1; let second = 2;
+let d = 1; d = 2;
+//let f = 1; let f = 2;
+//const r = 1; r = 2;
+//const t = 1, t = 2;*/
+
+
+/*console.log(typeof('4' * '9'));*/
+
+/*let rs = 'true';
+console.log (typeof (rs));*/
+
+/*console.log(Boolean (0));
+console.log    (Boolean   (''));
+console.log    (Boolean  ({}));
+console.log   (Boolean   (undefined));
+console.log  ( Boolean  (NaN));
+console.log  (Boolean  (null));
+console.log   (Boolean  ([]));*/
